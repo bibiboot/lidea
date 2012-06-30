f1f03988e11b0193484a9d2fb17043b54b484b7b
dan-boa <rehman.d.fazlur@gmail.com>
2012-06-30 13:45:48 +0530

    Sentence return format changes

--- ideacalculator/views.py
+++ ideacalculator/views.py
@@ -1,13 +1,17 @@
-from ideacalculator.models import PlaceType, TypeRelation
-from lidea.places.models import Attribute as Place
-from ideacalculator.utils import getNearbyPlaces, getPlaceCombs, getWeightage, getSentence, getPlaces
-from django.utils import simplejson
+import pdb
 from operator import itemgetter
+from django.utils import simplejson
 from django.shortcuts import render_to_response
+from lidea.places.models import Attribute as Place
 from django.views.decorators.csrf import csrf_exempt
+from ideacalculator.models import PlaceType, TypeRelation
+from ideacalculator.utils import getNearbyPlaces, getPlaceCombs, getWeightage, getPlaces
 
 @csrf_exempt
 def getideas(request):
+    """
+    Calculate the idea + verb and emit
+    """
     if request.method.upper() == 'GET':
         try:
             osm_id = request.GET['osmid']
@@ -20,13 +24,17 @@ def getideas(request):
         for placeComb in placeCombs:
             p1, p2 = getPlaces(placeComb)
             points, factor = getWeightage(origin, placeComb)
-            sentence = getSentence(p1, p2, factor)
+            factors = [ factor, 'factor1', 'factor2' ]
+            verb1, verb2 = 'verb1','verb2'
             if (points >0):
                 ideas.append({
-                        'place1': p1.name,
-                        'place2': p2.name,
-                        'points': points,
-                        'sentence': sentence})
-        ideas.sort(key=itemgetter('points'))
-        ideas.reverse()
+                        'place1' : p1.name,
+                        'place2' : p2.name,
+                        'verb1'  : verb1,
+                        'verb2'  : verb2,
+                        'factors': factors,
+                        'points' : points,
+                        })
+        # Sort according to the points
+        ideas.sort(key=itemgetter('points'), reverse=True)
         return render_to_response('ideacalculator/idea.html', {'ideas': ideas})
