255abca85158edc76ebdd2c73c559a59637e7e40
dan-boa <rehman.d.fazlur@gmail.com>
2012-06-30 14:32:40 +0530

    Adding crawler

--- event/bms.py
+++ event/bms.py
@@ -9,74 +9,90 @@ class BMS():
     def __init__(self):
         # set up a web scraping session
         self.sess = dryscrape.Session(base_url = 'http://in.bookmyshow.com')
-
-        # there are some failing HTTP requests, so we need to enter
-        # a more error-resistant mode (like real browsers do)
         self.sess.set_error_tolerant(True)
-
-        # we don't need images
         self.sess.set_attribute('auto_load_images', False)
-
-        # if we wanted, we could also configure a proxy server to use,
-        # so we can for example use Fiddler to monitor the requests
-        # performed by this script
         #sess.set_proxy('10.80.2.13', 80)
-
-        # visit homepage and log in
         self.sess.visit('/')
 
         self.r = get_cache('event')
+      
 
-    def _region(self):
+    def crawl(self):
+        for region in self._regions():
+            print '-----------------'
+            print region.text()
+            region.click()
+                 
+            for event in self._events():
+                print event.text()
+                event.click()
+                
+                for day in self._days():
+                    print day.text()
+                    pdb.set_trace()
+                    day.click()
+                    self.snapshot(title=day.text())
+                """
+                    for cinemahall in self._cinemahalls():
+                        print cinemahall.text()
+                        cinemahall.click()
+                        
+                        for movie_timing in self._movies_timings():
+                            self._select_movie_timing(movie_timing)
+                            self.sess.render('sample1.png')
+                """ 
+
+    def snapshot(self, title):
+        self.sess.render("%s.png" %title)
+
+    def _regions(self, key='region'):
         # Select all region list
         region_label = self.sess.at_xpath('//li[@id="cntRegion"]')
         region_label.at_xpath('div/input').click()
+        return region_label.xpath('div[@class="ddrrl"]/ul/li/div/span')
 
-        regions = region_label.xpath('div[@class="ddrrl"]/ul/li/div')
-        pdb.set_trace()
-        [ self.r._client.lpush('regions',pickle.dumps(region)) for region in regions ]
+    def _events(self):
+        # Event will be automatically bi clicked
+        event = self.sess.at_xpath('//li[@id="cntEvent"]')
+        return event.xpath('div[@class="ddrrl"]/ul/li/div/span')
+        
+    def _days(self):
+        # Day will be automatically bi clicked
+        day = self.sess.at_xpath('//li[@id="cntDate"]')
+        return day.xpath('div[@class="ddrrl"]/ul/li/div/span')
 
-    def _select_region(self):
-        # Click on one
-        region = pickle.load( self.r._client.lpop('regions') )
-        pdb.set_trace()
-        region.at_xpath('span').click()
-        self.sess.render('sample.jpg')
-
-    def defi(self):
-
-        event = sess.at_xpath('//li[@id="cntEvent"]')
-        oneevent = event.at_xpath('div[@class="ddrrl"]/ul/li/div')
-        # Click on one event
-        oneevent.at_xpath('span').click()
-
-        day = sess.at_xpath('//li[@id="cntDate"]')
-        #onedate = day.at_xpath('div[@class="ddrrl"]/ul/li/div')
-        onedate = day.xpath('div[@class="ddrrl"]/ul/li/div')[1]
-        # Click on one day
-        onedate.at_xpath('span').click()
-
+    def _cinemahalls(self):
         # New page
-        onecinemahall = sess.at_xpath('//div[@class="dl"]/h5')
-        # Click on the cinema hall
-        sess.at_xpath('//div[@class="dl"]/h5/a').click()
-
+        #return self.sess.at_xpath('//div[@class="dl"]/h5')
+        clist = self.sess.xpath('//div[@id="dShowTimes"]/div[@class="btc"]/div[@class="dl"]/h5/a')
+        pdb.set_trace()
+        return clist
+        
+    def _movies_timings(self):
         # Cinema hall page
-        onemovie = sess.at_xpath('//div[@class="btc"]/div[@class="dl"]/h5')
-        onetiming = sess.at_xpath('//div[@class="btc"]/div[@class="dr"]/a')
-        onetiming.click()
+        return self.sess.xpath('//div[@id="dShowTimes"]/div[@class="btc"]')
+        
+    def _select_movie_timing(self, movie_time):
+        movie = movie_time.at_xpath('div[@class="dl"]/h5/a')
+        print movie.text()
+        """
+        for timing in movie_time.xpath('div[@class="dr"]/a'):
+            timing.click()
+            # Sleep for some time
+            time.sleep(30)
+        """
 
+    def _select_passenger_class(self):
         # Select passenger page
-        time.sleep(10)
-        time.sleep(20)
-        sess.at_xpath('//select[@id="tikCat"]').set('0002')
-        sess.at_xpath('//select[@id="tikCat"]').select_option()
-        sess.at_xpath('//select[@id="cmbQty"]/option').set_attr('value','1')
-        sess.at_xpath('//a[@id="chseats"]').click()
+        self.sess.at_xpath('//select[@id="tikCat"]').set('0002')
+        self.sess.at_xpath('//select[@id="tikCat"]').select_option()
+        self.sess.at_xpath('//select[@id="cmbQty"]/option').set_attr('value','1')
+        self.sess.at_xpath('//a[@id="chseats"]').click()
 
+    def _seat_avail(self):
         # Seat layout page
         time.sleep(20)
-        seat_table = sess.at_xpath('//div[@id="tblSeatInfo"]/table/tbody')
+        seat_table = self.sess.at_xpath('//div[@id="tblSeatInfo"]/table/tbody')
         print len(seat_table.xpath('tr'))
 
         for row in seat_table.xpath('tr'):
@@ -93,5 +109,4 @@ class BMS():
             len(tb.xpath('tr')[1].xpath('td/img'))
 
 bms = BMS()
-bms._region()
-bms._select_region()
+bms.crawl()
